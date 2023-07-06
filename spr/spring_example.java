f2c860bcc5c8962acb3d29e240367fd970c1327d
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-07-06 13:21:16 +0000

    0

---
+++ spr/spring_example.java
@@ -0,0 +1,19 @@
+//https://www.javatpoint.com/steps-to-create-spring-application
+
+package com.javatpoint;
+
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Hello: " + name);
+    }
+}
