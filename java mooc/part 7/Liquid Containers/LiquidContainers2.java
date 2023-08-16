e9ffe0aabe5204e8db2c8aa1fdc1ba74d81b1126
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-08-16 13:56:18 +0300

    Update LiquidContainers2.java

--- java mooc/part 7/Liquid Containers/LiquidContainers2.java
+++ java mooc/part 7/Liquid Containers/LiquidContainers2.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+public class LiquidContainers2 {
+public static void main(String[] args){
+  Scanner scanner = new Scanner(System.in);
+  Container firstContainer = new Container();
+  Container secondContainer = new Container();
+  while (true) {
+    System.out.println("First: " + firstContainer);
+    System.out.println("Second: " + secondContainer);
 
+    String input = scanner.nextLine();
+    if(input.equals(("quit")){
+      break;
+    }
+    String[] parts = input.split(" ");
+    String command = parts[0];
+    int amount = INteger.parseInt(parts[1]);
+    if(command.equals("add")){
+      firstContainer.add(amount);
+    }
+    else if(command.equals("move")){
+      if(amount <=firstContainer.contains()){
+        firstContainer.remove(amount);
+        secondContainer.add(amount);
+      } else{
+        secondContainer.add(firstContainer.contains());
+        firstContainer.remove(firstContainer.contains());
+      } else if(command.equals("remove")){
+      secondContainer.remove(amount);
+    }
+  }
+}
+}
